ab170e829e5e7c14a92cd8e917bb61a37c4c2bf7
Aleksei900200 <ally.kawa14@yahoo.com>
2024-09-03 15:56:47 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector('.j-btn-test').addEventListener('click', function () {
+  document.querySelector('.btn-icon_on').classList.toggle('hidden');
+  document.querySelector('.btn-icon_off').classList.toggle('hidden');
+});
